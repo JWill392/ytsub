7693f2f48168b7cac4b0c22b09b7de6dbc5a0cd4
Jackson Williams <jackson.r.williams+github@gmail.com>
2013-01-01 04:06:35 -0800

    Basic, slow sub querying

---
+++ youtubelist/main.py
@@ -0,0 +1,47 @@
+#  main.py
+#  This file is part of youtube-list 
+# 
+#  Copyright (C) 2013 - Jackson Williams
+
+#  youtube-list is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  youtube-list is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with youtube-list.  If not, see <http://www.gnu.org/licenses/>.
+
+import httplib2
+import api
+
+from apiclient.discovery import build
+from credentials import acquire_credentials
+
+def main():
+    CLIENT_SECRETS_FILE = "../data/client_secrets.json"
+    SCOPES = "https://www.googleapis.com/auth/youtube"
+    YOUTUBE_API_SERVICE_NAME = "youtube"
+    YOUTUBE_API_VERSION = "v3"
+
+    credentials = acquire_credentials(SCOPES, CLIENT_SECRETS_FILE)
+
+    youtube = build(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION,
+        http=credentials.authorize(httplib2.Http()))
+        
+    watched = api.get_watched_ids(youtube)
+    
+    new = api.get_sub_vids(youtube)
+    
+    unwatched_new = filter(lambda x: x.id not in watched, new)
+    unwatched_new.sort(reverse=True)
+    for v in unwatched_new:
+        print v
+
+if __name__ == '__main__':
+    main()
+
---
+++ youtubelist/video.py
@@ -0,0 +1,46 @@
+#  video.py
+#  This file is part of youtube-list 
+# 
+#  Copyright (C) 2013 - Jackson Williams
+
+#  youtube-list is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#
+#  youtube-list is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with youtube-list.  If not, see <http://www.gnu.org/licenses/>.
+
+from functools import total_ordering
+from datetime import datetime
+from datetime import timedelta
+
+@total_ordering
+class Vid:
+    def __init__(self, author_name, playlist_item_video):
+        self.id = playlist_item_video["snippet"]["resourceId"]["videoId"]
+        self.title = playlist_item_video["snippet"]["title"]
+        self.date = datetime.strptime(\
+                playlist_item_video["snippet"]["publishedAt"],\
+                "%Y-%m-%dT%H:%M:%S.000Z")
+        self.author = author_name
+        
+    def __str__(self):
+        return "Vid{id:"+self.id+ \
+                ", date:"+self.date.isoformat()+ \
+                ", title:"+self.title+ \
+                ", author:"+self.author+"}"
+        
+    def __eq__(self, other):
+        return self.id == other.id
+        
+    def __lt__(self, other):
+        return self.date < other.date
+        
+    def __hash__(self):
+        return hash(self.id)
